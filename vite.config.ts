2df4c85401ab426d2f98c80a87614b42bf00dec4
zhuling904 <ling.zhu.904@gmail.com>
2024-08-03 13:19:08 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import autoprefixer from 'autoprefixer';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import autoprefixer from "autoprefixer";
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr()
-  ],
-  root: path.resolve(__dirname, 'src'),
+  plugins: [react(), svgr()],
+  root: path.resolve(__dirname, "src"),
+  base: "/", // 设置基础路径为根目录
   css: {
     // 进行 PostCSS 配置
     postcss: {
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+        }),
+      ],
     },
     //* css模块化
-    modules: { // css模块化 文件以.module.[css|less|scss]结尾
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-      hashPrefix: 'prefix',
+    modules: {
+      // css模块化 文件以.module.[css|less|scss]结尾
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+      hashPrefix: "prefix",
     },
     //* 预编译支持less
     preprocessorOptions: {
@@ -36,10 +35,9 @@ export default defineConfig({
   resolve: {
     // 别名配置
     alias: {
-      '@assets': path.join(__dirname, 'src/assets')
-    }
+      "@assets": path.join(__dirname, "src/assets"),
+    },
   },
-})
-
+});
 
 // 生产环境打包完预览产物"preview": "vite preview"
